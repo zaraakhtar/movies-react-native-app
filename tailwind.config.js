e34edbaddce30ba38eddfc1f4c6c59885c65b118
zaraakhtar <zaramalikawan9@gmail.com>
2025-11-09 23:57:58 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#F59E0B",
+        accent: "#10B981",
+      },
+    },
+  },
+  plugins: [],
+}
